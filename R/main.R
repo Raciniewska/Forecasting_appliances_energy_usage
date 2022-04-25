bc0b523df2a2387492f9f63d0c7966e15f5e4c2f
Demongo2009 <kuba.galat1@wp.pl>
2022-04-25 21:08:55 +0200

    main code skeleton

---
+++ R/main.R
@@ -0,0 +1,14 @@
+read.csv("./data/energydata_complete.csv")
+
+
+# reading the data
+
+# data analysis
+
+# data processing
+
+# model creation and training
+
+# validation and tuning
+
+# model score
